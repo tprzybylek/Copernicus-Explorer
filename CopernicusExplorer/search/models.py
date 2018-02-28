ce548be8696cd63687730ab8a2a777a8e7d0365b
tprzybylek <tajemniczyjogurt@gmail.com>
2018-02-28 22:35:18 +0100

    configure PostGIS DB, create models

---
+++ CopernicusExplorer/search/models.py
@@ -0,0 +1,34 @@
+#from django.db import models
+from django.contrib.gis.db import models
+
+# Create your models here.
+
+class ImageS1(models.Model):
+    id = models.CharField(max_length=36, primary_key=True)
+    title = models.CharField(max_length=67)
+    ingestiondate = models.DateTimeField()
+    satellite = models.CharField(max_length=3)
+    mode = models.CharField(max_length=2)
+    orbitdirection = models.CharField(max_length=10)
+    polarisationmode = models.CharField(max_length=5)
+    producttype = models.CharField(max_length=3)
+    relativeorbitnumber = models.IntegerField()
+    size =  models.IntegerField()
+    coordinates =  models.PolygonField()
+    def __str__(self):
+        return self.id
+
+# class ImageS2(models.Model):
+#     id = models.CharField(max_length=36, primary_key=True)
+#     title = models.CharField(max_length=60)
+#     ingestiondate = models.DateTimeField()
+#     satellite = models.CharField(max_length=3)
+#     mode = models.CharField(max_length=8)
+#     orbitdirection = models.CharField(max_length=10)
+#     cloudcover = models.DecimalField(max_digits=5, decimal_places=2)
+#     producttype = models.CharField(max_length=7)
+#     relativeorbitnumber = models.IntegerField()
+#     size =  models.IntegerField()
+#     coordinates =  models.PolygonField()
+#     def __str__(self):
+#         return self.id
