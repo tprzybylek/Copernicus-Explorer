dbce63769191b104a32dcd5111e24fda51c70f47
tprzybylek <tajemniczyjogurt@gmail.com>
2018-03-19 08:49:49 +0100

    Change database schema

--- CopernicusExplorer/search/urls.py
+++ CopernicusExplorer/search/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-
+from django.urls import path, include
 from . import views
 
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('product', views.SearchView)
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
     path('results/', views.results, name='results'),
+    path('', include(router.urls))
 ]
