25613f2e149acf649a7eaabacf8ca9830af0f94f
tprzybylek <tajemniczyjogurt@gmail.com>
2018-04-05 10:44:53 +0200

    Add add, remove and show_all views

--- CopernicusExplorer/order/views.py
+++ CopernicusExplorer/order/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from order.models import Product
+from .cart import Cart
+from django.http import HttpResponse
+
+# @require_POST
 
 # Create your views here.
 
-def cart(request):
-    return render(request, 'order/cart.html')
+
+def cart_add(request):
+    id = request.POST['id']
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=id)
+    cart.add(product)
+    return HttpResponse("")
+    # return redirect('cart_show_all')
+
+
+def cart_remove(request):
+    id = request.POST['id']
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=id)
+    cart.remove(product)
+    return HttpResponse("")
+
+def cart_clear(request):
+    # TODO empty cart
+    return HttpResponse("")
+
+
+def cart_show_all(request):
+    cart = Cart(request)
+    cart_items = Product.objects.filter(id__in = cart.cart['products'])
+    return render(request, 'order/cart.html', {'cart': cart_items})
