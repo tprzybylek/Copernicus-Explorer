3e144eaf4c00c5c4eeff31c9998f3c7ef0019a18
tprzybylek <tajemniczyjogurt@gmail.com>
2018-06-26 11:13:42 +0200

    Remove ProductOrder table from the Admin view

--- CopernicusExplorer/order/admin.py
+++ CopernicusExplorer/order/admin.py
@@ -9,4 +9,3 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('pk', 'ordered_date_time', 'completed_date_time', 'status')
 
 admin.site.register(Order, OrderAdmin)
-admin.site.register(ProductOrder)
