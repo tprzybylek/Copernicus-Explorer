75edb1aa6ea5021209dec300d972f98e9fd0c86d
tprzybylek <tajemniczyjogurt@gmail.com>
2018-06-26 11:19:14 +0200

    Add static url

--- CopernicusExplorer/CopernicusExplorer/settings.py
+++ CopernicusExplorer/CopernicusExplorer/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'search.apps.SearchConfig',
     'order.apps.OrderConfig',
-    'rest_framework'
 ]
 
 REST_FRAMEWORK = {
@@ -148,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "CopernicusExplorer/static")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATIC_URL = '/static/'
 
