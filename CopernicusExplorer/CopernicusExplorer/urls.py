66eba7f68e0261ae508f93a6a9b0aa2430b52bca
tprzybylek <tajemniczyjogurt@gmail.com>
2018-06-27 08:53:16 +0200

    Fix static files settings

--- CopernicusExplorer/CopernicusExplorer/urls.py
+++ CopernicusExplorer/CopernicusExplorer/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('search.urls')),
     path('admin/', admin.site.urls),
     path('search/', include('search.urls')),
     path('order/', include('order.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + staticfiles_urlpatterns()
