da1c0406153ba62d0c7cbfd28099e4ca767ba573
tprzybylek <tajemniczyjogurt@gmail.com>
2018-06-27 09:04:13 +0200

    Apply patch

--- CopernicusExplorer/CopernicusExplorer/celery.py
+++ CopernicusExplorer/CopernicusExplorer/celery.py
@@ -1,11 +1,12 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CopernicusExplorer.settings')
 
-app = Celery('CopernicusExplorer', broker='amqp://localhost//')
+app = Celery('CopernicusExplorer', broker='amqp://guest:guest@localhost:5672//')
 
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
